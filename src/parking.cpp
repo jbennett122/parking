9140d36852382a6e722c92440baab69976b77e15
jbennett <jbennett@cs.odu.edu>
2015-04-05 08:58:20 -0400

    initial commit

---
+++ src/parking.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : parking.cpp
+// Author      : Justin Bennett
+// Version     : 1
+// Copyright   : This belongs to justin
+// Description : Parking simulation
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
